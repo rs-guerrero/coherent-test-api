23d5776c109d7b5b27282aff19525c4ec9b03197
rs-guerrero <guerreros_24@hotmail.com>
2023-12-12 12:43:54 -0600

    My first commit

---
+++ src/main/java/com/coherent/test/task/CoherentTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.coherent.test.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoherentTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoherentTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coherent/test/task/CoherentTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coherent.test.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoherentTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
